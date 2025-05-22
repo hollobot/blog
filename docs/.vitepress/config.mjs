6ee146d21d2080b1cb20952ae105b59a3cc246f7
hello <2380983020@qq.com>
2025-05-22 20:57:50 +0800

    init blog

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,127 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "hello blog",
+  description: "code blog",
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicon-180x180.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-180x180.png",
+      },
+    ],
+    ["link", { rel: "shortcut icon", href: "/favicon-16x16.ico" }],
+  ],
+  outDir: ".vitepress/dist",
+  srcDir: "src",
+  themeConfig: {
+    search: {
+      provider: "local",
+      options: {
+        miniSearch: {
+          options: {
+            /* ... */
+          },
+          searchOptions: {
+            /* ... */
+          },
+        },
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "没有找到结果",
+            resetButtonTitle: "清除搜索条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+              closeText: "关闭",
+            },
+          },
+        },
+      },
+    },
+
+    logo: "/logo.png",
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+      { text: "java", link: "https://docs.ffffee.com" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+          { text: "vue快速入门", link: "/Vue3快速上手" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+
+    footer: {
+      message: `<a href="https://beian.miit.gov.cn/" target="_blank">京ICP备20016634号-2</a>`,
+      copyright: `版权所有 © 2019-${new Date().getFullYear()} hello`,
+    },
+
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+
+    // https://vitepress.dev/zh/reference/default-theme-config#outline
+    outline: {
+      level: [2, 3],
+      label: "页面导航",
+    },
+
+    lastUpdated: {
+      text: "最后更新于",
+      formatOptions: {
+        dateStyle: "short", // full
+        timeStyle: "short", // medium
+      },
+    },
+
+    langMenuLabel: "多语言",
+    returnToTopLabel: "回到顶部",
+    sidebarMenuLabel: "菜单",
+    darkModeSwitchLabel: "主题",
+    lightModeSwitchTitle: "切换到浅色模式",
+    darkModeSwitchTitle: "切换到深色模式",
+  },
+
+  vite: {
+    // https://cn.vitejs.dev/config/shared-options.html#publicdir
+    publicDir: "../src/public", // 指定 public 目录路径
+  },
+});
---
+++ docs/src/index.md
@@ -0,0 +1,46 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "hello blog"
+  image:
+    src: /logo.png
+    alt: hello
+  text: "Electron桌面开发"
+  tagline: Electron-vite即时通讯，校园外卖，spring boot，redis，nety
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: vue3 快速入门
+      link: /Vue3快速上手.md
+
+features:
+  - title: Electron vlc
+    details: electron集成libVLC音视频播放器，支持音视频解码播放
+  - title: Electron screenshot
+    details: 使用c++ addon插件实现截图功能，速度是毫秒级的
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+  - title: Electron line
+    details: Electron集成line插件，集成谷歌应用商店插件到你的electron应用中
+    
+---

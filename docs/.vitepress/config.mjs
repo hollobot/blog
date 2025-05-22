f149726049baa5d8655d5a5a24851c980c0c7dec
hello <2380983020@qq.com>
2025-05-22 23:00:36 +0800

    恢复图片、样式

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "./blog",
   title: "hello blog",
   description: "code blog",
   head: [
